6686df569adc40687a1c3f581ed2f4d94a746276
Àlex torregrosa <alextorre98@gmail.com>
2023-01-15 22:25:02 +0100

    Style updates to the LFSR code

--- design/ifu/lfsr_prng.sv
+++ design/ifu/lfsr_prng.sv
@@ -24,12 +24,11 @@ module lfsr_prng
     import swerv_types::*;
     # (
         parameter SIZE = 3
-				//parameter INIT_VAL = 0
     ) (
-        input logic  clk ,
-        input logic rst_l,
-				input logic [63:0] seed_i,
-        output logic [SIZE-1 : 0] output_number_o 
+        input  logic            clk ,
+        input  logic            rst_l,
+        input  logic [63:0]     seed_i,
+        output logic [SIZE-1:0] output_number_o
     );
 
     // 64-bit XNOR LFSR (0 is a valid state)
@@ -37,24 +36,16 @@ module lfsr_prng
     // taps = 64,63,61,60
     logic [63:0] lfsr, lfsr_next;
     logic newbit;
-//WORD_SIZE'(32'b00100110011111101110111100010000)
 
-`ifndef SYNTHESIS
-	always_ff @(posedge clk or negedge rst_l) begin
-		if (rst_l == 0)
-  		lfsr <= seed_i;
-	  else
-  	  lfsr <= lfsr_next;
-	  end
-`else
-    rvdff #(64) random_seed_ff (.*, .clk(clk), .din(lfsr_next), .dout(lfsr));
-`endif
+    always_ff @(posedge clk or negedge rst_l) begin
+        if (rst_l == 0)
+          lfsr <= seed_i;
+        else
+          lfsr <= lfsr_next;
+    end
 
-
-    assign newbit = ~(lfsr[63] ^ lfsr[62] ^ lfsr[60] ^ lfsr[59]);
-    assign lfsr_next = {lfsr[62:0], newbit};
+    assign newbit          = ~(lfsr[63] ^ lfsr[62] ^ lfsr[60] ^ lfsr[59]);
+    assign lfsr_next       =  {lfsr[62:0], newbit};
     assign output_number_o = lfsr[SIZE-1:0];
-
-
 
 endmodule
